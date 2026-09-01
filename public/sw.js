6a93f5334bd6fc2fa0a378f191f8e729589f63cf
Acep Yudi Heryadi <yudivisual.a11y@gmail.com>
2026-09-01 17:03:32 +0700

    feat: pasang PWA WebAPK Manifest dan Service Worker agar dapat diinstal sebagai Aplikasi Android resmi dengan izin Download penuh

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// NYARIOS Service Worker for PWA WebAPK
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let network handle fetching
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
